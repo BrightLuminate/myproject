5e25477423678d8b465fad1905e452785bb310f9
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-17 17:49:59 +0900

    Delete myproject/myapp/ko_serializers.py

--- myproject/myapp/ko_serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Images
-
-class TestDataSerializer(ModelSerializer):
-    class Meta:
-        model = Images
-        fields = '__all__'
