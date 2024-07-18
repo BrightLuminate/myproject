8c6ce697f4b195f04642520711a574ddd8accdbe
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-18 16:34:00 +0900

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views 
+from .views import createTestDatas
 
 urlpatterns = [
     path('upload/', views.upload_image, name='upload_image'),
@@ -7,6 +8,8 @@ urlpatterns = [
     path('image_success/<int:image_id>/', views.image_success, name='image_success'),
     path('show_image/', views.show_image, name='show_image'),
     path('images/', views.image, name='images'),
-    # ko
+    path('create/', createTestDatas, name='createTestDatas'),
+    path('images/', include('rest_framework.urls')),
     path('test01datas/<int:id>/', views.getTestDatas, name="test01datas"),
+
 ]
