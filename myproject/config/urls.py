382fa8ae0d39b6a9b38a815cf7dfd7ecdc47a9a6
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-19 12:49:20 +0900

    Update urls.py

--- myproject/config/urls.py
+++ myproject/config/urls.py
@@ -7,13 +7,9 @@ urlpatterns = [
     path('mail/', include('mail.urls')),
     path('send/', views.sendEmail, name='send'),
     path('', views.signin, name='signin'),
-    path('signout/', views.signout, name='signout'),
     path('profile/', views.profile, name='profile'),
     path('mail/send/', views.send_mail, name='send_mail'),
     path('', include('myapp.urls')),  # myapp의 URL 설정을 포함합니다.
-    # path('pybo/', include('pybo.urls')),
-    # path('signup/', views.signup, name='signup'),
-    # path('home/', views.home, name='home'),
 ]
 
 
