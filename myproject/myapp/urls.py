2d155cf733479dfbd625aae43cf97b0fe22084b9
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-19 09:16:29 +0900

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views 
+from .views import createTestDatas
 
 urlpatterns = [
     path('upload/', views.upload_image, name='upload_image'),
