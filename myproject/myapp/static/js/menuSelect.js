caea258ee79d4e13260ab25229f2ae08190604f2
joungeun <167954005+joungeun@users.noreply.github.com>
2024-07-23 10:25:22 +0900

    Update menuSelect.js

--- myproject/myapp/static/js/menuSelect.js
+++ myproject/myapp/static/js/menuSelect.js
@@ -1,8 +1,15 @@
 
 function setActiveMenu(index){
-    // $(menus[index]).removeClass( "menu_on" );
 
     const menus = $('a.menu')
-    $(menus[index]).removeClass( "menu" );
+    $(menus[index]).removeClass("menu")
     $(menus[index]).addClass("menu_on")
+
+    if (index === 0) {
+        $("#title").text("모니터링");
+    } else if (index === 1) {
+        $("#title").text("기기상태");
+    } else if (index === 2) {
+        $("#title").text("품질관리");
+    }
 }
