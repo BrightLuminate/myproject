8642d40fa5d7aaf70b0c17305dcc4d75a7cf9dde
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-23 15:30:19 +0900

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,18 +4,18 @@ from .views import createTestDatas
 
 urlpatterns = [
     # 테스트 파일
-    path('upload/', views.upload_image, name='upload_image'),
-    path('success/', views.image_success, name='image_success'),
-    path('image_success/<int:image_id>/', views.image_success, name='image_success'),
-    path('show_image/', views.show_image, name='show_image'),
-    path('images/', views.image, name='images'),
+    # path('upload/', views.upload_image, name='upload_image'),
+    # path('success/', views.image_success, name='image_success'),
+    # path('image_success/<int:image_id>/', views.image_success, name='image_success'),
+    # path('show_image/', views.show_image, name='show_image'),
+    # path('images/', views.image, name='images'),
 
     # 기기제어, 품질검사, 로그기록 페이지
     path('profile/deviceState/', views.deviceState, name='deviceState'),
     path('profile/qualityInspect/', views.qualityInspect, name='qualityInspect'),
     path('profile/logRecord/', views.logRecord, name='logRecord'),
     
-    # ko
+    # get 
     path('test01datas/<int:id>/', views.getTestDatas, name="test01datas"),
     path('test01date/', views.getTestDate, name="test01date"),
     path('test01week/', views.getTestweek, name="test01week"),
