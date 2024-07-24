9b9a1e1dd8b07f08738d10894578c72c60c6b267
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-24 13:27:55 +0900

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -9,8 +9,9 @@ class ImageModel(models.Model):
 class Images(models.Model):
     Detection_Time = models.DateTimeField(auto_now_add=True)
     image_name = models.CharField(max_length=255)
+    Pull_Measurement = models.CharField(max_length=255)
     coustomer = models.TextField(max_length=255)
     image_url = models.URLField()
 
     def __str__(self):
-        return f'{self.Detection_Time} : {self.image_name} : {self.coustomer} : {self.image_url}'
+        return f'{self.Detection_Time} : {self.image_name} : {self.Pull_Measurement} :  {self.coustomer} : {self.image_url}'
