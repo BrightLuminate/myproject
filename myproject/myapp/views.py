ea7e084fd8e0900a86e5ed634b0a0324f56f8edc
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-07-24 16:21:57 +0900

    Update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -58,9 +58,9 @@ def signin(request):
 def profile(request):
     return render(request, 'profile.html')
 
-def signout(request):
-    logout(request)
-    return redirect('/')
+# def signout(request):
+#     logout(request)
+#     return redirect('/')
 
 def email_view(request):
     return render(request, 'email.html')
