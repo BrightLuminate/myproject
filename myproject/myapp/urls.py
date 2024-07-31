dc7bb759a40e032a9e81069ccbb9c8bbeebfa867
joungeun <167954005+joungeun@users.noreply.github.com>
2024-07-31 12:33:32 +0900

    2024-07-31 12/23

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,27 +3,11 @@ from . import views
 from .views import createTestDatas
 
 urlpatterns = [
-    # 테스트 파일
-    # path('upload/', views.upload_image, name='upload_image'),
-    # path('success/', views.image_success, name='image_success'),
-    # path('image_success/<int:image_id>/', views.image_success, name='image_success'),
-    # path('show_image/', views.show_image, name='show_image'),
-    # path('images/', views.image, name='images'),
-
     # 기기제어, 품질검사, 로그기록 페이지
     path('profile/deviceState/', views.deviceState, name='deviceState'),
     path('profile/qualityInspect/', views.qualityInspect, name='qualityInspect'),
     path('profile/logRecord/', views.logRecord, name='logRecord'),
     
-
-    # Test
-    
-    #  페이징
-    path('show_image/', views.show_image, name='show_image'),
-    #  스크롤
-    path('pos/', views.show_video, name='pos'),
-
-
     # get 
     path('test01datas/<int:id>/', views.getTestDatas, name="test01datas"),
     path('test01date/', views.getTestDate, name="test01date"),
