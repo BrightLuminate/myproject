2e47239fc41b095320a538d1b953619e4ce1a80f
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-08-06 12:16:49 +0900

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from . import views 
 from .views import createTestDatas
+from .views import get_chart_data
 
 urlpatterns = [
+   
+
     # 기기제어, 품질검사, 로그기록 페이지
     path('profile/deviceState/', views.deviceState, name='deviceState'),
     path('profile/qualityInspect/', views.qualityInspect, name='qualityInspect'),
     path('profile/logRecord/', views.logRecord, name='logRecord'),
     
+
     # get 
     path('test01datas/<int:id>/', views.getTestDatas, name="test01datas"),
     path('test01date/', views.getTestDate, name="test01date"),
@@ -18,4 +22,5 @@ urlpatterns = [
     # post
     path('create/', createTestDatas, name='createTestDatas'),
     path('images/', include('rest_framework.urls')),
+    path('api/chart-data/', get_chart_data, name='get_chart_data'),
 ]
