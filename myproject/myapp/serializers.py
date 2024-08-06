2e12ea72bee85c5338e1b850e229deda6bc22830
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-08-06 14:04:10 +0900

    Update

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -11,9 +11,17 @@ class TestDataSerializer(serializers.ModelSerializer):
 
 class TestDataSerializer(serializers.ModelSerializer):
 
-    Detection_Time = serializers.DateTimeField(format="%m-%d %H:%M:%S")
-    # Detection_Time = serializers.DateTimeField(format="%Y-%m-%d")
+    Detection_Time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    # Detection_Time = serializers.DateTimeField(format="%H:%M:%S")
     class Meta:
         model = Images
         fields = '__all__'
+
+    def get_image_url(self, obj):
+        return obj.image.url
     
+
+class MyAppImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = '__all__'
