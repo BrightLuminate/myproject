c3b493b671b2a4bcfe88b91860b100e3aecfc350
jungheam <165759200+jungheam@users.noreply.github.com>
2024-08-06 09:55:02 +0900

    Update urls.py

--- myproject/config/urls.py
+++ myproject/config/urls.py
@@ -7,10 +7,8 @@ urlpatterns = [
     path('mail/', include('mail.urls')),
     path('send/', views.sendEmail, name='send'),
     path('', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
     path('profile/', views.profile, name='profile'),
     path('mail/send/', views.send_mail, name='send_mail'),
     path('', include('myapp.urls')),  # myapp의 URL 설정을 포함합니다.
 ]
-
-
-
