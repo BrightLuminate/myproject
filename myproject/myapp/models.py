cc19094d0e5e99183ed43e582c0034716de314b6
joungeun <167954005+joungeun@users.noreply.github.com>
2024-08-13 19:45:33 +0900

    도현씨 소스랑 합침

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,15 +6,16 @@ class ImageModel(models.Model):
     def __str__(self):
         return self.name
 
+
 class Images(models.Model):
     Detection_Time = models.DateTimeField(auto_now_add=True)
-    image_name = models.CharField(max_length=255)
-    Pull_Measurement = models.CharField(max_length=255)
-    coustomer = models.TextField(max_length=255)
-    image_url = models.URLField()
-    category = models.CharField(max_length=255)
-#  default='unknown'
+    image_name = models.CharField(max_length=255, null=True, blank=True)
+    defect_types = models.CharField(max_length=255, null=True, blank=True)
+    Pull_Measurement = models.CharField(max_length=255, default="Steel 10*5°4, 1000.RPM")
+    image_url = models.URLField(max_length=255, null=True, blank=True)  # Correct way to store image paths
+    classification = models.CharField(max_length=50, default='ok_front')
+    timestamp = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=255, null=True, blank=True)
+
     def __str__(self):
-        return f'{self.Detection_Time} : {self.image_name} : {self.Pull_Measurement} :  {self.coustomer} : {self.image_url}'
-
-
+        return f'{self.Detection_Time} : {self.image_name}: {self.defect_types} : {self.Pull_Measurement} : {self.customer} : {self.image_url} : {self.classification} : {self.timestamp} : {self.category}'
