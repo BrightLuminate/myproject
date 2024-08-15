978cdfe4a27e36aa872b5b50f4d8ebbb344682da
joungeun <167954005+joungeun@users.noreply.github.com>
2024-08-15 13:04:16 +0900

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -14,8 +14,7 @@ class Images(models.Model):
     Pull_Measurement = models.CharField(max_length=255, default="Steel 10*5°4, 1000.RPM")
     image_url = models.URLField(max_length=255, null=True, blank=True)  # Correct way to store image paths
     classification = models.CharField(max_length=50, default='ok_front')
-    timestamp = models.DateTimeField(auto_now=True)
     category = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.Detection_Time} : {self.image_name}: {self.defect_types} : {self.Pull_Measurement} : {self.customer} : {self.image_url} : {self.classification} : {self.timestamp} : {self.category}'
+        return f'{self.Detection_Time} : {self.image_name}: {self.defect_types} : {self.Pull_Measurement} : {self.image_url} : {self.classification}  : {self.category}'
